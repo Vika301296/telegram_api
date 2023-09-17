57e41e034da4920949f5474b12db9acf15a898e8
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-09-18 02:56:19 +0300

    publish photo to Telegram channel

--- pictures_bot.py
+++ pictures_bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 bot = telegram.Bot(token=os.getenv('TOKEN'))
 chat_id = 986055024
 chat_id_1 = '@nasa_and_spacex_pictures'
-bot.send_message(text='Hi Vika!', chat_id=chat_id_1)
+# bot.send_message(text='Hi Vika!', chat_id=chat_id_1)
+URL = 'https://cdn2.thecatapi.com/images/3dl.jpg'
 
-updates = bot.get_updates()
-print(updates[0])
+bot.send_photo(chat_id_1, URL)
