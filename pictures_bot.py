5d0d78464989c74e0989ae3499e4d079f4ea205e
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-09-20 21:56:21 +0300

    DRY the code

--- pictures_bot.py
+++ pictures_bot.py
@@ -13,19 +13,13 @@ def main():
     sleeping_time = 4*60*60
     images = os.listdir('images')
     bot = telegram.Bot(token=telegram_token)
-    last_sent_index = 0
     while True:
         for index, image_name in enumerate(images):
             image_path = os.path.join('images', image_name)
             with open(image_path, 'rb') as image_file:
                 bot.send_photo(chat_id=telegram_chat_id, photo=image_file)
-                last_sent_index += 1
                 time.sleep(sleeping_time)
-                last_sent_index = index
-            if last_sent_index >= len(images):
-                print('All images have been sent.')
-                random.shuffle(images)
-                last_sent_index = 0
+            random.shuffle(images)
 
 
 if __name__ == '__main__':
